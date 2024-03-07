48363999f75c753dbd0e79a40abbdc82c47321d9
cilinmengye <834430027@qq.com>
2024-03-07 18:02:55 +0800

    install 2 chapter pa to local

--- Chapter2/questuion_2-5.c
+++ Chapter2/questuion_2-5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef unsigned char *byte_pointer;
 
@@ -19,6 +20,10 @@ int main()
     int val=0x87654321;
     byte_pointer valp=(byte_pointer) &val;
     show_bytes(valp,1);
+    const char *s="abcdef";
+    show_bytes((byte_pointer)s,strlen(s));
+    unsigned ulength=0;
+    int length=0;
+    printf("%x,%x\n",ulength-1,length-1);
     return 0;
 }
-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
