ea54bea524d66cd42721f1d1a4896f344e12d465
cilinmengye <834430027@qq.com>
2024-03-23 22:56:29 +0800

    start Shell Lab

---
+++ Shell_Lab/Test/argcvTest.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[], char *envp[]){
+
+    printf("%d\n",argc);
+    for (int i = 0; i < argc; i++){
+        printf("%s\n",argv[i]);
+    }
+    return 0;
+}
